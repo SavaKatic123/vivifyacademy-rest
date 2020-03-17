b0871967c9c337712fa389379f255aac9f861067
Sava Katic <sava@Savas-MacBook-Pro.local>
2020-03-17 22:31:46 +0100

    init

--- vivify-academy-vue/src/App.vue
+++ vivify-academy-vue/src/App.vue
@@ -6,12 +6,12 @@
 </template>
 
 <script>
-import NavBar from './components/NavBar.vue'
+import NavBar from './components/NavBar.vue';
 
 export default {
   name: 'app',
   components: {
     NavBar
   }
-}
+};
 </script>
